6b7944ea0269a5fe0ba74a5c9948e2f0fef27505
Hecateo <juniorcubamontero83@gmail.com>
2025-11-05 11:01:41 -0500

    avance
    avance de difuminado al caer derrotado

--- Assets/Scripts/Cambios/CambiarReiniciar.cs
+++ Assets/Scripts/Cambios/CambiarReiniciar.cs
@@ -1,13 +1,44 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class CambiarReiniciar : MonoBehaviour
 {
+    public GameObject gameObjectOscurecer;
+    public RawImage Oscurecer;
+    public float TOscurecer = 2f;
+
+    public void OscurecerEscena()
+    {
+        gameObjectOscurecer.SetActive(true);
+        StartCoroutine(oscuro());
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            SceneManager.LoadScene("Reinicar");
+            OscurecerEscena();
         }
+    }
+
+    IEnumerator oscuro()
+    {
+        float tiempo = 0;
+        Color oscurecer = Oscurecer.color;
+
+        // Ir subiendo el alpha de 0 → 1
+        while (tiempo < TOscurecer)
+        {
+            tiempo += Time.deltaTime;
+            oscurecer.a = Mathf.Lerp(0, 1, tiempo / TOscurecer);
+            Oscurecer.color = oscurecer;
+            yield return null;
+        }
+
+        Application.Quit();
+
+        SceneManager.LoadScene("Reinicar");
     }
 }
